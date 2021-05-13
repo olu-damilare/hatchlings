231c470c883db6ba927f48a17e6e2e9910b86601
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-13 07:45:32 +0100

    Update

--- tests/airlineReservationAndBookingTests.py
+++ tests/airlineReservationAndBookingTests.py
@@ -4,6 +4,7 @@ from airlineReservationAndBooking.aeroplane import Aeroplane
 from airlineReservationAndBooking.Passenger import Passenger
 from airlineReservationAndBooking.reservation import Reservation
 from airlineReservationAndBooking.SeatClass import SeatClass
+from airlineReservationAndBooking.flightbooking import FlightBooking
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,6 +13,12 @@ class MyTestCase(unittest.TestCase):
         self.aeroplane = Aeroplane("Eagle squad")
         self.airline = Airline("Imperial Airline", self.aeroplane)
         self.reservation = Reservation()
+        self.passenger = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        self.flight_booking = FlightBooking()
+
+    def tearDown(self) -> None:
+        self.reservation.empty_reservation_list()
+        self.flight_booking.empty_booked_list()
 
     def test_that_airline_can_be_created(self):
         self.assertEqual("Airline name: Imperial Airline" +
@@ -27,8 +34,147 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(50, self.aeroplane.get_total_number_of_seats())
 
     def test_first_class_reservation(self):
-        self.passenger = Passenger("Olu Jola", "0000", "bina@jolo.com")
-        self.reserve = self.reservation.reserve_flight(self.passenger, SeatClass.first_class)
+        self.reservation.reserve_flight(self.passenger, SeatClass.first_class)
         self.assertTrue(self.reservation.has_reserved(self.passenger))
         self.assertEqual(1, self.reservation.get_reservation_ID(self.passenger))
         self.assertEqual("FIRSTCLASS", self.reservation.get_reserved_seat_class(self.passenger))
+
+    def test_business_class_reservation(self):
+        self.reservation.reserve_flight(self.passenger, SeatClass.business)
+        self.assertTrue(self.reservation.has_reserved(self.passenger))
+        self.assertEqual(1, self.reservation.get_reservation_ID(self.passenger))
+        self.assertEqual("BUSINESS", self.reservation.get_reserved_seat_class(self.passenger))
+
+    def test_economy_class_reservation(self):
+        self.reservation.reserve_flight(self.passenger, SeatClass.economy)
+        self.assertTrue(self.reservation.has_reserved(self.passenger))
+        self.assertEqual(1, self.reservation.get_reservation_ID(self.passenger))
+        self.assertEqual("ECONOMY", self.reservation.get_reserved_seat_class(self.passenger))
+
+    def test_multiple_passenger_can_reserve_first_class(self):
+        self.passenger1 = Passenger("Ade Bajomo", "23543", "dbomo@gmail.com")
+        self.reservation.reserve_flight(self.passenger, SeatClass.first_class)
+        self.reservation.reserve_flight(self.passenger1, SeatClass.first_class)
+        self.assertTrue(self.reservation.has_reserved(self.passenger))
+        self.assertTrue(self.reservation.has_reserved(self.passenger1))
+        self.assertEqual(1, self.reservation.get_reservation_ID(self.passenger))
+        self.assertEqual(2, self.reservation.get_reservation_ID(self.passenger1))
+        self.assertEqual("FIRSTCLASS", self.reservation.get_reserved_seat_class(self.passenger))
+        self.assertEqual("FIRSTCLASS", self.reservation.get_reserved_seat_class(self.passenger1))
+        self.assertEqual(2, self.reservation.get_total_number_reserved_seats())
+
+    def test_multiple_passenger_can_reserve_business_class(self):
+        self.passenger1 = Passenger("Ade Bajomo", "23543", "dbomo@gmail.com")
+        self.reservation.reserve_flight(self.passenger, SeatClass.business)
+        self.reservation.reserve_flight(self.passenger1, SeatClass.business)
+        self.assertTrue(self.reservation.has_reserved(self.passenger))
+        self.assertTrue(self.reservation.has_reserved(self.passenger1))
+        self.assertEqual(1, self.reservation.get_reservation_ID(self.passenger))
+        self.assertEqual(2, self.reservation.get_reservation_ID(self.passenger1))
+        self.assertEqual("BUSINESS", self.reservation.get_reserved_seat_class(self.passenger))
+        self.assertEqual("BUSINESS", self.reservation.get_reserved_seat_class(self.passenger1))
+        self.assertEqual(2, self.reservation.get_total_number_reserved_seats())
+
+    def test_multiple_passenger_can_reserve_economy_class(self):
+        self.passenger1 = Passenger("Ade Bajomo", "23543", "dbomo@gmail.com")
+        self.reservation.reserve_flight(self.passenger, SeatClass.economy)
+        self.reservation.reserve_flight(self.passenger1, SeatClass.economy)
+        self.assertTrue(self.reservation.has_reserved(self.passenger))
+        self.assertTrue(self.reservation.has_reserved(self.passenger1))
+        self.assertEqual(1, self.reservation.get_reservation_ID(self.passenger))
+        self.assertEqual(2, self.reservation.get_reservation_ID(self.passenger1))
+        self.assertEqual("ECONOMY", self.reservation.get_reserved_seat_class(self.passenger))
+        self.assertEqual("ECONOMY", self.reservation.get_reserved_seat_class(self.passenger1))
+        self.assertEqual(2, self.reservation.get_total_number_reserved_seats())
+
+    def test_that_passenger_can_cancel_reservation(self):
+        self.reservation.reserve_flight(self.passenger, SeatClass.economy)
+        self.assertTrue(self.reservation.has_reserved(self.passenger))
+        self.assertEqual(1, self.reservation.get_reservation_ID(self.passenger))
+        self.assertEqual("ECONOMY", self.reservation.get_reserved_seat_class(self.passenger))
+        reservation_id = self.reservation.get_reservation_ID(self.passenger)
+        self.reservation.cancelReservation(reservation_id)
+        self.assertFalse(self.reservation.has_reserved(self.passenger))
+        self.assertEqual(0, self.reservation.get_total_number_reserved_seats())
+        self.assertIsNone(self.reservation.get_reserved_seat_class(self.passenger))
+
+    def test_that_passenger_can_book_first_class_with_reservation_id(self):
+        self.reservation.reserve_flight(self.passenger, SeatClass.first_class)
+        self.assertEqual(1, self.reservation.get_total_number_reserved_seats())
+        reservation_id = self.reservation.get_reservation_ID(self.passenger)
+        self.flight_booking.book_with_reservation_id(reservation_id)
+        self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+        self.assertEqual("FIRSTCLASS", FlightBooking.get_booked_seat_type(self.passenger))
+
+    def test_that_passenger_can_book_economy_class_with_reservation_id(self):
+            self.reservation.reserve_flight(self.passenger, SeatClass.economy)
+            self.assertEqual(1, self.reservation.get_total_number_reserved_seats())
+            reservation_id = self.reservation.get_reservation_ID(self.passenger)
+            self.flight_booking.book_with_reservation_id(reservation_id)
+            self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+            self.assertEqual("ECONOMY", FlightBooking.get_booked_seat_type(self.passenger))
+
+    def test_that_passenger_can_book_business_class_with_reservation_id(self):
+        self.reservation.reserve_flight(self.passenger, SeatClass.business)
+        self.assertEqual(1, self.reservation.get_total_number_reserved_seats())
+        reservation_id = self.reservation.get_reservation_ID(self.passenger)
+        self.flight_booking.book_with_reservation_id(reservation_id)
+        self.assertEqual(1, self.flight_booking.get_total_number_of_business_class_seats_booked())
+        self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+        self.assertEqual("BUSINESS", FlightBooking.get_booked_seat_type(self.passenger))
+
+    def test_that_same_reservation_id_cannot_be_used_to_book_a_flight_twice(self):
+        self.reservation.reserve_flight(self.passenger, SeatClass.first_class)
+        self.assertEqual(1, self.reservation.get_total_number_reserved_seats())
+        reservation_id = self.reservation.get_reservation_ID(self.passenger)
+        self.flight_booking.book_with_reservation_id(reservation_id)
+        self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+        self.assertEqual("FIRSTCLASS", FlightBooking.get_booked_seat_type(self.passenger))
+
+        self.flight_booking.book_with_reservation_id(reservation_id)
+        self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+
+    def test_that_passenger_can_book_first_class_without_first_reserving_seat(self):
+        self.flight_booking.book_flight(self.passenger, SeatClass.first_class)
+        self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+        self.assertEqual("FIRSTCLASS", FlightBooking.get_booked_seat_type(self.passenger))
+
+    def test_that_passenger_can_book_business_class_without_first_reserving_seat(self):
+        self.flight_booking.book_flight(self.passenger, SeatClass.business)
+        self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+        self.assertEqual("BUSINESS", FlightBooking.get_booked_seat_type(self.passenger))
+
+    def test_that_passenger_can_book_economy_class_without_first_reserving_seat(self):
+        self.flight_booking.book_flight(self.passenger, SeatClass.economy)
+        self.assertEqual(1, self.flight_booking.get_total_count_of_seats_booked())
+        self.assertEqual("ECONOMY", FlightBooking.get_booked_seat_type(self.passenger))
+
+    def test_that_only_ten_first_class_seats_can_be_booked(self):
+        passenger_1 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_2 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_3 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_4 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_5 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_6 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_7 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_8 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_9 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_10 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+        passenger_11 = Passenger("Olu Jola", "0000", "bina@jolo.com")
+
+        self.flight_booking.book_flight(passenger_1, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_2, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_3, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_4, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_5, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_6, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_7, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_8, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_9, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_10, SeatClass.first_class)
+        self.flight_booking.book_flight(passenger_11, SeatClass.first_class)
+
+        self.assertEqual(10, self.flight_booking.get_total_number_of_first_class_seats_booked())
+        self.assertEqual(10, self.flight_booking.get_total_count_of_seats_booked())
+
+
